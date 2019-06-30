0473a9540850400f3b4d3155e7c42fcbf5e35baf
dyanneo <dyanneo@yahoo.com>
2019-06-29 20:49:51 -0500

    adds getters and setters

--- menu_project.js
+++ menu_project.js
@@ -4,4 +4,24 @@ const menu = {
       mains: [],
       desserts: [],
     },
+    get appetizers() {
+      // console.log("top of get appetizers");
+      return this._courses.appetizers;
+    },
+    get mains() {
+      return this._courses.mains;
+    },
+    get desserts() {
+      return this._courses.desserts;
+    },
+    set appetizers(appetizerIn) {
+      this._courses.appetizers = appetizerIn;
+    },
+    set mains(mainIn) {
+      this._courses.mains = mainIn;
+    }, 
+    set desserts(dessertIn) {
+      this._courses.desserts = dessertIn;
+    },
+    
 };
