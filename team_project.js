e56f48aa858d14bdbffc2c2b50ef3a9ae225de7d
dyanneo <dyanneo@yahoo.com>
2019-06-29 21:35:41 -0500

    adds getters

--- team_project.js
+++ team_project.js
@@ -16,6 +16,30 @@ const team = {
         age: 30
       },   
   ],
-  _games: [],
-
+  _games: [
+    {
+        opponent: 'Raiders',
+        teamPoints: 0,
+        opponentPoints: 13
+      },
+      {
+        opponent: 'Crusaders',
+        teamPoints: 35,
+        opponentPoints: 7
+      },
+      {
+        opponent: 'Heat',
+        teamPoints: 3,
+        opponentPoints: 3
+      },
+  ],
+  get players() {
+    return this._players;
+  },
+  get games() {
+    return this._games;
+  },
 };
+
+console.log(team.players);
+console.log(team.games);
