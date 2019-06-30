53d60d956478854d27df5511e92f144821c9325c
dyanneo <dyanneo@yahoo.com>
2019-06-29 21:24:38 -0500

    adds players

---
+++ team_project.js
@@ -0,0 +1,21 @@
+const team = {
+  _players: [
+    {
+        firstName: 'Maya', 
+        lastName: 'Hamm', 
+        age: 35
+      },
+      {
+        firstName: 'Martina', 
+        lastName: 'Navratilova', 
+        age: 60
+      },
+      {
+        firstName: 'Venus', 
+        lastName: 'Williams', 
+        age: 30
+      },   
+  ],
+  _games: [],
+
+};
