6a866cef5d8df3c0ccf80a5770b3024de0d6b30b
dyanneo <dyanneo@yahoo.com>
2019-06-30 20:07:31 -0500

    adds book and movie clases

--- library_project.js
+++ library_project.js
@@ -45,27 +45,76 @@ class Media {
     constructor(title) {
         this._title = title;
         this._isCheckedOut = false;
-        this._ratings = [];
+        this._ratings = [1,3,5];
     }
 
   get title() {
     return this._title;
   }
+
   get isCheckedOut() {
     return this._isCheckedOut;
   }
+  
   get ratings() {
     return this._ratings;
   }
   toggleCheckOutStatus() {
     if (this._isCheckedOut) {
       !this._isCheckedOut;
-    }
-    if (!this._isCheckedOut) {
+    } else {
       this._isCheckedOut = true;
     }
   }
+  
   set isCheckedOut(out) {
     this._isCheckedOut = out;
   }
+
+  getAverageRating() {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    const ratingsList = this.ratings;
+    const reduced = ratingsList.reduce(reducer);
+    const avgRating = reduced / ratingsList.length;
+    return avgRating;
+  }
+  addRating(num) {
+    this._ratings.push(num);
+  }
 }
+class Book extends Media {
+    constructor(title,author,pages) {
+        super(title);
+        this._author = author;
+        this._pages = pages;
+
+    }
+    get author() {
+        return this._author;
+    }
+    get pages() {
+        return this._pages;
+    }
+}
+class Movie extends Media {
+    constructor(title,director,runTime) {
+        super(title);
+        this._director = director;
+        this._runTime = runTime;
+
+    }
+    get director() {
+        return this._director;
+    }
+    get runTime() {
+        return this._runTime;
+    }
+}
+
+historyOfEverything = new Book('A Short History of Everything', 'Bill Bryson', 544);
+historyOfEverything.toggleCheckOutStatus();
+console.log(historyOfEverything);
+/*
+const book = new Media('The Mists of Avalon');
+console.log(book.getAverageRating());
+*/
